9ef603b094316602d6316f8367734d2110ef98db
AlexKomanov <akomanov88@gmail.com>
2024-12-22 19:52:19 +0200

    adding lesson 3

---
+++ lesson_03/input_basics.py
@@ -0,0 +1,10 @@
+username = float(input("Enter your username: "))
+# print(username)
+print(f"Hello {username}, how are you today?")
+
+
+# age = float(input("Enter your age: "))
+# print(age)
+# print(type(age))
+#
+# print(age+1)
