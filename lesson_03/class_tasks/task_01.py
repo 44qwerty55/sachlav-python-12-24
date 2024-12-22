de73371f0375b090837fec4bcc123f23e1455769
AlexKomanov <akomanov88@gmail.com>
2024-12-22 20:44:42 +0200

    lesson 03 second part

---
+++ lesson_03/class_tasks/task_01.py
@@ -0,0 +1,2 @@
+a = int(input('Sample input: '))
+print(a + 1)
---
+++ lesson_03/print_with_end.py
@@ -0,0 +1,5 @@
+print('a1', 'b2', 'c3', 'd4', 'e5', end="-- \t\n\t --")
+print('a1', 'b2', 'c3', 'd4', 'e5' )
+print('a1', 'b2', 'c3', 'd4', 'e5', end="\t\n\t", sep="-<>-")
+print('a1', 'b2', 'c3', 'd4', 'e5' )
+
