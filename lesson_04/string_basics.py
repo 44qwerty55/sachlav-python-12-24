61acb2365c7e85ff6e3ee41b27c5efd73132cabd
AlexKomanov <akomanov88@gmail.com>
2024-12-29 21:14:37 +0200

    lesson 04 - string basics

---
+++ lesson_04/string_basics.py
@@ -0,0 +1,29 @@
+name = "Alex"
+name_1 = 'Alex'
+name_2 = f"Alex" # f-string
+name_3 =  """ 
+Alex 
+"""
+
+# print("Abc" + "def")
+# print("***" * 3)
+#
+# print("A" in "Alexander")
+# print("a" in "Alex")
+# print("a" in "Alex".lower()) # "Alex".lower() -> alex
+#
+# print(len("abcdefg"))
+# print(len("Alex"))
+# print(len([]))
+
+# print("a" == "A")
+# print(ord("a"))
+# print(ord("A"))
+# print(ord("b"))
+# print(ord("B"))
+# print("a" > "A")
+# print("ab" > "aB")
+
+my_name = "Alexander"
+print(my_name[2])
+my_name[-2] = "r"
